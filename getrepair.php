b41a7eba8ac7d7fe0f368970f9e8d150914dc79c
CodeLavel Thailand <codelavel@gmail.com>
2020-10-23 20:43:22 +0700

    update repairlist

--- getrepair.php
+++ getrepair.php
@@ -2,11 +2,30 @@
     include 'connect.php';
 
     $sql=$conn->query("SELECT * FROM repair WHERE rp_id");
+    $data;
+    $datatest;
+    $json_data='{"data":[';
 
+while($data=$sql->fetch_assoc()){
+    //print_r($data);
+    $json_data.=json_encode($data).',';
+    //$datatest = json_encode($data);
+    //echo json_encode($data);
+}
+$json_data= chop($json_data,",");
+$json_data.=']}';
+echo $json_data;
+/*
+    while($row = mysqli_fetch_array($sql)){
+
+
+    }*/
+/*
     $data=$sql->fetch_assoc();
-
-    echo json_encode($data);
-
+    
+    print_r(json_encode($data));
+    //echo json_encode($data);
+*/
     mysqli_close($conn);
 
 ?>
--- lib/pages/ListRepairPage.dart
+++ lib/pages/ListRepairPage.dart
@@ -11,18 +11,34 @@ class ListRepairPage extends StatefulWidget {
 }
 
 class _ListRepairPageState extends State<ListRepairPage> {
+  var respondList = null;
   Repair repair;
-  Future<Repair> getRepair() async {
+  //List<Repair> repairList = new List<Repair>();
+  List<DataRow> listRow = new List<DataRow>();
+  Future<List<DataRow>> getRepair() async {
     await http
         .get("http://192.168.1.10/FlutterITM/getrepair.php")
         .then((response) {
       if (jsonDecode(response.body) != null) {
         setState(() {
-          repair = Repair.fromJson(jsonDecode(response.body));
+          respondList = jsonDecode(response.body)["data"];
+          for (var i = 0; i < respondList.length; i++) {
+            repair = Repair.fromJson(respondList[i]);
+            listRow.add(DataRow(
+              cells: <DataCell>[
+                DataCell(Text(
+                    "${repair.id}")), //Extracting from Map element the value
+                DataCell(Text("${repair.rpCode}")),
+                DataCell(Text("${repair.acID}")),
+                DataCell(Text("${repair.rpStatus}")),
+              ],
+            ));
+            //repairList.add(repair);
+          }
         });
       }
     });
-    return repair;
+    return listRow;
   }
 
   @override
@@ -39,28 +55,29 @@ class _ListRepairPageState extends State<ListRepairPage> {
           title: Text('รายการแจ้งซ่อม'),
         ),
         body: ListView(children: <Widget>[
-          DataTable(columns: [
-            DataColumn(label: Text('#')),
-            DataColumn(label: Text('รหัสแจ้งซ่อม')),
-            DataColumn(label: Text('รหัสพนักงาน')),
-            DataColumn(label: Text('เลขครุภัณฑ์')),
-            DataColumn(label: Text('สถานะ')),
-            DataColumn(label: Text('วันที่แจ้ง')),
-            DataColumn(label: Text('รายละเอียด')),
-          ], rows: [
+          DataTable(
+              columns: [
+                DataColumn(label: Text('#')),
+                DataColumn(label: Text('รหัสแจ้งซ่อม')),
+                DataColumn(label: Text('เลขครุภัณฑ์')),
+                DataColumn(label: Text('สถานะ')),
+              ],
+              rows:
+                  listRow /*[
             DataRow(
               cells: <DataCell>[
                 DataCell(Text(
-                    "${repair.id}")), //Extracting from Map element the value
-                DataCell(Text("${repair.rpCode}")),
-                DataCell(Text("${repair.empCode}")),
-                DataCell(Text("${repair.acID}")),
-                DataCell(Text("${repair.rpStatus}")),
-                DataCell(Text("${repair.rpDate}")),
-                DataCell(Text("${repair.rpDetail}")),
+                    "${repairList[0].id}")), //Extracting from Map element the value
+                DataCell(Text("${repairList[0].rpCode}")),
+                DataCell(Text("${repairList[0].empCode}")),
+                DataCell(Text("${repairList[0].acID}")),
+                DataCell(Text("${repairList[0].rpStatus}")),
+                DataCell(Text("${repairList[0].rpDate}")),
+                DataCell(Text("${repairList[0].rpDetail}")),
               ],
             ),
-          ]),
+          ]*/
+              ),
         ]));
   }
 }
